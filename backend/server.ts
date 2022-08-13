42ffbd0c3fa3613cc9339edf17966c05332afb99
kkata <14849535+kkata@users.noreply.github.com>
2022-08-13 18:45:14 +0900

    add routes and controller

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import router from "./routes/userRoutes";
 
 const PORT = process.env.PORT || 8000;
 
@@ -10,6 +11,9 @@ app.get("/", (_req, res) => {
     message: "Hello World",
   });
 });
+
+// Routes
+app.use("/api/users", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
