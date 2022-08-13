f7c62dca6df7df474462f30415ccef57560cb6ae
kkata <14849535+kkata@users.noreply.github.com>
2022-08-13 13:10:43 +0900

    basic express server setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,16 @@
-console.log("Server...");
+import express from "express";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
