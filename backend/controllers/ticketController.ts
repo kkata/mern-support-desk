79c5784b1854f7dcb96fa70cad7154bb944e3dd4
kkata <14849535+kkata@users.noreply.github.com>
2022-08-18 17:45:18 +0900

    single ticket, update and delete (backend)

--- backend/controllers/ticketController.ts
+++ backend/controllers/ticketController.ts
@@ -23,6 +23,36 @@ export const getTickets: RequestHandler = asyncHandler(
   }
 );
 
+// @desc Get user tickets
+// @route GET /api/tickets/:id
+// @access Private
+export const getTicket: RequestHandler = asyncHandler(
+  async (req, res, _next) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      res.status(404);
+      throw new Error("Ticket not found");
+    }
+
+    if (String(ticket.user!) !== user._id.toString()) {
+      //              ^^^^^toString: {}; ????
+      res.status(401);
+      throw new Error("Unauthorized");
+    }
+
+    res.status(200).json(ticket);
+  }
+);
+
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
@@ -51,5 +81,73 @@ export const createTicket: RequestHandler = asyncHandler(
     });
 
     res.status(200).json(ticket);
+  }
+);
+
+// @desc Delete user tickets
+// @route DELETE /api/tickets/:id
+// @access Private
+export const deleteTicket: RequestHandler = asyncHandler(
+  async (req, res, _next) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      res.status(404);
+      throw new Error("Ticket not found");
+    }
+
+    if (String(ticket.user!) !== user._id.toString()) {
+      //              ^^^^^toString: {}; ????
+      res.status(401);
+      throw new Error("Unauthorized");
+    }
+
+    await ticket.remove();
+
+    res.status(200).json({ success: true });
+  }
+);
+
+// @desc Update user tickets
+// @route PUT /api/tickets/:id
+// @access Private
+export const updateTicket: RequestHandler = asyncHandler(
+  async (req, res, _next) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      res.status(404);
+      throw new Error("Ticket not found");
+    }
+
+    if (String(ticket.user!) !== user._id.toString()) {
+      //              ^^^^^toString: {}; ????
+      res.status(401);
+      throw new Error("Unauthorized");
+    }
+
+    const updateTicket = await Ticket.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updateTicket);
   }
 );
--- backend/routes/ticketRoutes.ts
+++ backend/routes/ticketRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 const router = Router();
-import { getTickets, createTicket } from "../controllers/ticketController";
+import {
+  getTickets,
+  getTicket,
+  createTicket,
+  updateTicket,
+  deleteTicket,
+} from "../controllers/ticketController";
 
 import { protect } from "../middleware/authMiddleware";
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 export const ticketRouter = router;
